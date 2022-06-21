95d11e510e5f6f23055fb9698971fea94e5bd1ff
iaht <klrt0120@gmail.com>
2022-06-21 23:57:30 +0700

    Cut Layout HTML CSS basic

---
+++ app.js
@@ -0,0 +1,10 @@
+$('.customer-main-lists').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots:true ,
+    autoplaySpeed: 2000,
+    prevArrow:`<button type='button' class='slick-prev pull-left'><img srcset="./images/prev.png 2x"/> </button>`,
+    nextArrow:`<button type='button' class='slick-next pull-right'><img srcset="./images/next.png 2x"/></button>`
+  });
+
